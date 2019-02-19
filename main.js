817e0ac0ed711b7e946abe0cb57830ce20eeb001
Cristhian <cristhianmorales18@gmail.com>
2019-02-18 20:30:47 -0600

    Auth working

--- main.js
+++ main.js
@@ -17,15 +17,17 @@ router.get('/', function(req, res) {
    res.send("Hello World!");
 });
 router.put('/credential', function(req, res) {
-   var key = req.body.key;
+   var lkey = req.body.key;
    var shared = req.body.shared_secret;
-   if(keys.includes(key)){
+   if(Object.keys(keys).filter(function(key){
+   		return keys[key] == lkey;
+   })){
 	   	res.status(403).send({
 	    	success: 'true',
 	    	message: 'Forbidden'
 	    });
    }else{
-   	keys.push(key)
+   	keys.push({key: lkey, shared_secret: shared})
    	res.status(204).send({
    		success: 'true',
    		message: 'key Stored'
@@ -41,15 +43,29 @@ router.post('/message', function(req,res){
 	console.log(req.header['X-Key']);
 	messages.push({message: msg, tags:tag});
 	console.log(messages)
-	res.status(200).send({
-		success: 'true',
-		identifier: messages.length
-	});
-})
+	if(auth(req,res,'/message')){
+		res.status(200).send({
+			success: 'true',
+			identifier: messages.length
+		});
+	}else {
+		res.status(403).send({
+			succes: 'true',
+			message: 'Auth error'
+		})
+	}
+});
 
 router.get('/message/:id', function(req,res){
-	res.status(200).send("message:" + messages[req.params.id].msg);
-})
+	if(auth(req,res,'/message')){
+		res.status(200).send("message:" + messages[req.params.id].msg);
+	}else {
+		res.status(403).send({
+			succes: 'true',
+			message: 'Auth error'
+		})
+	}
+});
 router.get('/message/:tag', function(req,res){
 	let result = [];
 	for(var i = 0; i < messages.length; i++)
@@ -59,10 +75,48 @@ router.get('/message/:tag', function(req,res){
 	    result.push(messages[i].tags.name);
 	  }
 	}
+	if(auth(req,res,'/message')){
+		res.status(200).send("Matching tags: " + result);
+	}else {
+		res.status(403).send({
+			succes: 'true',
+			message: 'Auth error'
+		})
+	}
+	
+});
 
-	res.status(200).send("Matching tags: " + result);
-})
+var auth = function(req,res, url){
+	var authorized = false;
+	if(keys.includes(req.header['X-Key'])){
+		if(keys.includes(req.header['X-Route']) && url.parse(req.url).pathname == url){
+			if(req.header['X-Signature']){
+				let word1 = req.body.msg;
+				let word2 = req.body.tags;
+				let mixedWords = sortString(word1?word1+';':''+ word2?word2+';':'' + req.header['X-Route']? req.header['X-Route'] + ";" : '');
+				return req.header['X-Signature'] == crypto.createHmac('sha256', keys[req.header['X-Key']].shared_secret).digest(hex);
+			}
+		}
+	}
+	return authorized;
+}
 
+// sort by name property
+function sortString(str){
+  var arr = str.split('');
+  var tmp;
+  for(var i = 0; i < arr.length; i++){
+    for(var j = i + 1; j < arr.length; j++){
+      /* if ASCII code greater then swap the elements position*/
+      if(arr[i] > arr[j]){
+        tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+      }
+    }
+  }
+  return arr.join('');
+}
 app.use(router);
 
 app.listen(3000, function() {
